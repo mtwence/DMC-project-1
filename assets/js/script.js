cbd2808024a33adadea7305c9f38e32e1cdc0310
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-08 15:32:36 -0400

    chenged utelly request from jquery to fetch format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+input = $("#input");
+Search = input.val();
+
+// function to add %20 inbetween spaces of searchs
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -16,17 +21,15 @@ fetch('https://movie-database-alternative.p.rapidapi.com/?s=%3CREQUIRED%3E&r=jso
 
 var imdbCode = []
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=" + imdbCode + "&source=imdb&country=us",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "a341e5528dmshfb626c60584810dp17d30ejsnb83576f61579",
-		"X-RapidAPI-Host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+const options1 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a341e5528dmshfb626c60584810dp17d30ejsnb83576f61579',
+		'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
 	}
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=' + imdbCode + '&source=imdb&country=us', options1)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
