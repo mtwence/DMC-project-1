9ad353ced71bcce807383b53d99abd74e40488bb
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-08 15:24:39 -0400

    fixed javascript merge issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,32 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var imdbCode = "tt8009428"
-
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'a341e5528dmshfb626c60584810dp17d30ejsnb83576f61579',
-
-		'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-    }
-};
-
-fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=' + imdbCode
-+ '&source=imdb&country=us', options)
-
 		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
 	}
 };
 
-fetch('https://movie-database-alternative.p.rapidapi.com/?s=American%20Psycho&r=json&page=1', options)
-
+fetch('https://movie-database-alternative.p.rapidapi.com/?s=%3CREQUIRED%3E&r=json&page=1', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
+
+
+var imdbCode = []
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=" + imdbCode + "&source=imdb&country=us",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "a341e5528dmshfb626c60584810dp17d30ejsnb83576f61579",
+		"X-RapidAPI-Host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
