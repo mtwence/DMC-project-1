453afcd8d1997bb8f9ff76b3f1dd8e79157ee8ad
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-09 16:46:03 -0400

    added for loop for iterating through all return imdb codes from MDA API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 var input = $("#input");
 var form = $("#form");
 var streamName = $("#name")
-var movieInput = "Morbius";
-
+// var movieInput = "Ba";
 // function to add %20 inbetween spaces of searchs for input into MDA API
 form.on("submit", function (x) {
 	x.preventDefault();
 	var search = input.val();
-	
+
 	var rep = / /gi;
 	var movieInput = search.replace(rep, "%20");
 	console.log(movieInput)
-});
+
 
 // Fetch request for Movie Database Alternative 
 const options = {
@@ -28,7 +27,7 @@ fetch('https://movie-database-alternative.p.rapidapi.com/?s=' + movieInput + '&r
 	}).then(function (data) {
 		console.log(data);
 		var IDstorage = []; // Array for storing IDs
-		for (var i = 0; i < data.Search.length-1; i++) {
+		for (var i = 0; i < data.Search.length - 1; i++) {
 			//Image link to the poster
 			var poster = data.Search[i].Poster;
 			console.log(poster);
@@ -54,26 +53,21 @@ fetch('https://movie-database-alternative.p.rapidapi.com/?s=' + movieInput + '&r
 				'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
 			}
 		};
-		
-		fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=' + IDstorage[0] + '&source=imdb&country=us', options1)
-			.then(function (res) {
-				return res.json();
-			}).then(function (data) {
-				console.log(data);
-				for (var i = 0; i < data.collection.locations.length-1; i++) {
-					var streamName = data.collection.locations[i].display_name;
-					console.log(streamName);
-					var icon = data.collection.locations[i].icon;
-					console.log(icon);
-					var link = data.collection.locations[i].url;
-					console.log(link);
-				}
-			})
+		for (var j = 0; j < IDstorage.length; j++) {
+			fetch('https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=' + IDstorage[j] + '&source=imdb&country=us', options1)
+				.then(function (res) {
+					return res.json();
+				}).then(function (data) {
+					console.log(data);
+					for (var i = 0; i < data.collection.locations.length - 1; i++) {
+						var streamName = data.collection.locations[i].display_name;
+						console.log(streamName);
+						var icon = data.collection.locations[i].icon;
+						console.log(icon);
+						var link = data.collection.locations[i].url;
+						console.log(link);
+					}
+				})
+		}
 	})
-	.catch(err => console.error(err));
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> b48c36419f8c471f92fe3a168f56ac9fc10d3285
+});
